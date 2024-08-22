85a1d772271cda56cdbfe3ef9ae108718d389e93
Ayeza <ayezabashir442@gmail.com>
2024-08-22 17:55:10 +0500

    testimonials

--- assets/ayeza.js
+++ assets/ayeza.js
@@ -5,6 +5,8 @@ const playVideo = document.querySelector('.play-video');
 const playVideo1 = document.querySelector('.play-vid');
 const popupVideo = document.querySelector('.bg-video-container');
 const closePopUp = document.querySelector('.bg-video-container .close-icon')
+const ellipses = document.querySelectorAll('.ellipses .ellipse');
+const testimonials = document.querySelectorAll('.testimonial');
 
 openMenu.addEventListener('click', () => {
     closeMenu.style.display = 'block'
@@ -26,3 +28,13 @@ playVideo1.addEventListener('click', () => {
     popupVideo.style.display = 'block'
 
 })
+
+ellipses.forEach((ellipse, index) => {
+    ellipse.addEventListener('click', () => {
+        ellipses.forEach(el => el.classList.remove('active'));
+        testimonials.forEach(testimonial => testimonial.classList.remove('active'));
+        ellipse.classList.add('active');
+        testimonials[index].classList.add('active');
+    });
+});
+
