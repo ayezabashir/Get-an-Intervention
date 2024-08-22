f6015628f50105bd9efe10180f8f31ce697a7da7
Ayeza <ayezabashir442@gmail.com>
2024-08-22 16:31:18 +0500

    video

--- assets/ayeza.js
+++ assets/ayeza.js
@@ -1,6 +1,11 @@
 const openMenu = document.querySelector('.open-menu');
 const closeMenu = document.querySelector('.close-menu');
 const menuItems = document.querySelector('.menu-items');
+const playVideo = document.querySelector('.play-video');
+const playVideo1 = document.querySelector('.play-vid');
+const popupVideo = document.querySelector('.bg-video-container');
+const closePopUp = document.querySelector('.bg-video-container .close-icon')
+
 openMenu.addEventListener('click', () => {
     closeMenu.style.display = 'block'
     openMenu.style.display = 'none'
@@ -11,13 +16,13 @@ closeMenu.addEventListener('click', () => {
     closeMenu.style.display = 'none'
     menuItems.classList.remove('active');
 })
-
-document.addEventListener('invalid', () => {
-    var customText = $(this).get(0);
-    customText.setCustomValidity('');
-    if (!customText.validity.valid) {
-        textfield.setCustomValidity('This field can’t be empty. Ayeza Please fill it in.');
-    }
+playVideo.addEventListener('click', () => {
+    popupVideo.style.display = 'block'
 })
+closePopUp.addEventListener('click', () => {
+    popupVideo.style.display = 'none'
+})
+playVideo1.addEventListener('click', () => {
+    popupVideo.style.display = 'block'
 
-
+})
