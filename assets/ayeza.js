6043ce09b24f9219058d59b06907ea61dd65c844
Ayeza <ayezabashir442@gmail.com>
2024-08-22 10:36:46 +0500

    hero section

--- assets/ayeza.js
+++ assets/ayeza.js
@@ -10,6 +10,13 @@ closeMenu.addEventListener('click', () => {
     openMenu.style.display = 'block'
     closeMenu.style.display = 'none'
     menuItems.classList.remove('active');
-
-
 })
+
+document.addEventListener('invalid', () => {
+    var customText = $(this).get(0);
+    customText.setCustomValidity('');
+    if (!customText.validity.valid) {
+        textfield.setCustomValidity('This field can’t be empty. Ayeza Please fill it in.');
+    }
+})
+
